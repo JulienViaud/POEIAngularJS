bb6e36cff0a0d00bf3e45004beac015fa594349b
JulienViaud <jul.viaud.pro@gmail.com>
2018-01-23 17:27:12 +0100

    Etape 6 - interim       var indexFilms = _.findIndex(vm.films, function(o) { return o.img == $event.target.src; });       vm.films[indexFilms].select = true;       //console.log($event.target.src);   } with '#' will be ignored, and an empty message aborts the commit.

--- project/src/app/exo03/exo03.controller.js
+++ project/src/app/exo03/exo03.controller.js
@@ -4,12 +4,20 @@
   function Exo03Ctrl($location) {
     var vm = this;
     vm.films = [
-	{name: 'Fast and Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/AkSMd8AXSpA1L1SbBPc2AfVJfmE.jpg', year: 2001},
-      {name: '2 Fast 2 Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/u1Snfl40rXmAJNiMyZUczUyFVuf.jpg', year: 2003},
-	{name: 'Fast & Furious: Tokyo Drift', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/4ceW7kWPAc2uFG5WzYDbP8Od3gw.jpg', year: 2006}
+	{select : false, name: 'Fast and Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/AkSMd8AXSpA1L1SbBPc2AfVJfmE.jpg', year: 2001},
+      {select : false, name: '2 Fast 2 Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/u1Snfl40rXmAJNiMyZUczUyFVuf.jpg', year: 2003},
+	{select : false, name: 'Fast & Furious: Tokyo Drift', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/4ceW7kWPAc2uFG5WzYDbP8Od3gw.jpg', year: 2006}
       ];
-    
+      vm.clic = clicPhoto;
+
+      function clicPhoto($event){
+            var indexFilms = _.findIndex(vm.films, function(o) { return o.img == $event.target.src; });
+            vm.films[indexFilms].select = true;
+            //console.log($event.target.src);
+      }
   }
+
+  
 
   Exo03Ctrl.$inject = ['$location'];
 
