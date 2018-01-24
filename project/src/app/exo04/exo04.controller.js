485dde72692c8df24bf60f12db849298f97ba2ff
JulienViaud <jul.viaud.pro@gmail.com>
2018-01-24 12:46:02 +0100

    Etape 03

--- project/src/app/exo04/exo04.controller.js
+++ project/src/app/exo04/exo04.controller.js
@@ -3,11 +3,18 @@
 
   function Exo04Ctrl($location) {
     var vm = this;
-    
-  }
+    vm.txt;
+    vm.clicBouton=clicBouton;
+
+    function clicBouton(){
+
+            console.log(vm.txt);
+    }
 
   Exo04Ctrl.$inject = ['$location'];
 
+
+}
   angular.module('daproject')
     .controller('Exo04Ctrl', Exo04Ctrl);
 
